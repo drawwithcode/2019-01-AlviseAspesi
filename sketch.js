66d8c2f99658163d7b363de54b85d2c144c86fe6
alviseaspesi <alviseaspesi@gmail.com>
2019-09-22 17:48:49 +0200

    2019-01

--- sketch.js
+++ sketch.js
@@ -4,9 +4,61 @@ function preload(){
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  angleMode(DEGREES)
   background("black");
+  frameRate(12);
 }
 
 function draw() {
-  ellipse(width/2, height/2, 50);
+  translate(width/2, height/2);
+
+//metà sopra
+  line(200*cos(frameCount*6+3), 200*sin(frameCount*6+3), 200, 0);
+
+  stroke(
+    lerpColor(
+      color("orange"),
+      color("green"),
+      frameCount/60)
+    );
+//
+
+//metà sotto
+  line(200*cos(frameCount*6), -200*sin(frameCount*6), 200, 0);
+
+  stroke(
+    lerpColor(color("green"),
+    color("orange"),
+    frameCount/60)
+  );
+
+//metà sopra
+  line(200*cos(frameCount*6+3), 200*sin(frameCount*6+3), 200, 0);
+
+  stroke(
+    lerpColor(
+      color("orange"),
+      color("green"),
+      frameCount/60)
+    );
+//
+
+//metà sotto
+  line(200*cos(frameCount*6), -200*sin(frameCount*6), 200, 0);
+
+  stroke(
+    lerpColor(color("green"),
+    color("orange"),
+    frameCount/60)
+  );
+
+  if(frameCount==60){
+    noLoop();
+  }
+
+
+  //
+
+
+
 }
