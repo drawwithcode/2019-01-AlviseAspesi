0a1517b2596b7df4cfa4b549a5836967f98c7fec
alviseaspesi <alviseaspesi@gmail.com>
2019-09-24 00:41:57 +0200

    update 6

--- sketch.js
+++ sketch.js
@@ -11,49 +11,66 @@ function setup() {
 
 function draw() {
   push();
+  translate(width/2, height/2);
+  rotate(90);
+
+  stroke(color(85, 87, 92));
+  line(100*cos(frameCount*6+3), 100*sin(frameCount*6+3), 100, 0);
+
+  stroke(color(85, 87, 92));
+  line(100*cos(frameCount*6), -100*sin(frameCount*6), 100, 0);
+  pop();
+  
+  push();
   translate(width/2-200,height/2);
   rotate(-90);
+
   stroke(200, 210, 174);
   line(0, 50, 50*sin(frameCount*3-2), -50*cos(frameCount*3-2));
+
   stroke(200, 210, 174);
   line(0, 50, 50*sin(-frameCount*3+1), -50*cos(-frameCount*3+1));
   pop();
+
+
   push();
   translate(width/2+200,height/2);
   rotate(90);
+
   stroke(200, 210, 174);
   line(0, 50, 50*sin(frameCount*3-2), -50*cos(frameCount*3-2));
+
   stroke(200, 210, 174);
   line(0, 50, 50*sin(-frameCount*3+1), -50*cos(-frameCount*3+1));
   pop();
+
+
   push();
   translate(width/2,height/2-200);
   rotate(0);
+
   stroke(200, 210, 174);
   line(0, 50, 50*sin(frameCount*3-2), -50*cos(frameCount*3-2));
+
   stroke(200, 210, 174);
   line(0, 50, 50*sin(-frameCount*3+1), -50*cos(-frameCount*3+1));
   pop();
+
+
   push();
   translate(width/2,height/2+200);
   rotate(180);
+
   stroke(200, 210, 174);
   line(0, 50, 50*sin(frameCount*3-2), -50*cos(frameCount*3-2));
+
   stroke(200, 210, 174);
   line(0, 50, 50*sin(-frameCount*3+1), -50*cos(-frameCount*3+1));
   pop();
+
+
 
   if(frameCount==60){
     noLoop()
   }
-  translate(width/2, height/2);
-  rotate(90);
-
-//metà sopra
-  stroke(color(85, 87, 92));
-  line(100*cos(frameCount*6+3), 100*sin(frameCount*6+3), 100, 0);
-
-//metà sotto
-  stroke(color(85, 87, 92));
-  line(100*cos(frameCount*6), -100*sin(frameCount*6), 100, 0);
 }
